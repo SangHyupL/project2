62104bc70d38194e3c8aaa1b46c7c26b58ea74a0
SangHyupL <pk625890@gmail.com>
2023-04-27 20:07:05 +0900

    feat:project2

---
+++ project2/plugins/mongodb.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const fp = require('fastify-plugin')
+
+require('dotenv').config()
+
+const {
+    MONGO_USERNAME,
+    MONGO_PASSWORD,
+    MONGO_HOSTNAME
+} = process.env
+
+/**
+ * This plugins adds some utilities to handle http errors
+ *
+ * @see https://github.com/fastify/fastify-sensible
+*/
+module.exports = fp(async function (fastify, opts) {
+    const url =`mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:27017/?authMechanism=DEFAULT`
+    console.log(url)
+  
+    fastify.register(require('@fastify/mongodb'), {
+      forceClose: true,
+      url: url
+    })
+})
