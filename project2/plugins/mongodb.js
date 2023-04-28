2a3f5545e15380f44b71944ee427d268db682b1e
SangHyupL <127010049+SangHyupL@users.noreply.github.com>
2023-04-28 20:06:13 +0900

    Update mongodb.js

--- project2/plugins/mongodb.js
+++ project2/plugins/mongodb.js
@@ -2,11 +2,9 @@
 
 const fp = require('fastify-plugin')
 
-const {
-    MONGO_USERNAME,
-    MONGO_PASSWORD,
-    MONGO_HOSTNAME
-} = process.env
+let user = process.env.MONGO_USERNAME
+let passwd = process.env.MONGO_PASSWORD
+let host = process.env.MONGO_HOSTNAME
 
 /**
  * This plugins adds some utilities to handle http errors
@@ -14,7 +12,7 @@ const {
  * @see https://github.com/fastify/fastify-sensible
 */
 module.exports = fp(async function (fastify, opts) {
-    const url =`mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:27017/?authMechanism=DEFAULT`
+    const url =`mongodb://${user}:${passwd}@${host}:27017/?authMechanism=DEFAULT`
     console.log(url)
   
     fastify.register(require('@fastify/mongodb'), {
